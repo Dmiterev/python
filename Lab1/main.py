75095faf32738dda8e8767f607f3d94f0077012a
Dmiterev <113352857+Dmiterev@users.noreply.github.com>
2022-11-09 11:33:22 +0400

    Добавил докстринги с описанием параметров и немного исправил код

--- Lab1/main.py
+++ Lab1/main.py
@@ -4,15 +4,13 @@ import shutil
 from bs4 import BeautifulSoup as BS
 
 
-def dataset_create(name1,name2):
+def dataset_create():
     """
     Проверяет существование папки dataset.
     Cоздает папку dataset и ее подпапки.
     """
     try:
         os.mkdir("dataset")
-        os.mkdir(f"dataset/{name1}")
-        os.mkdir(f"dataset/{name2}")
     except OSError:
         shutil.rmtree("dataset")
         os.mkdir("dataset")
@@ -21,6 +19,10 @@ def dataset_create(name1,name2):
 def store_image(image_url, index, path):
     """
    Сохраняет полученное изображение в подпапку dataset и дает ему название согласно индексу.
+
+   :param image_url: ссылка на изображение
+   :param index: индекс изображения
+   :param path: путь к папке
     """
     saved_image = requests.get(f"https:{image_url}").content
     file = f"{path}/{index:04d}.jpg"
@@ -29,16 +31,22 @@ def store_image(image_url, index, path):
         save.close()
 
 
-def download_images(name, n, path):
+def download_images(name, n, path, URL = "https://yandex.ru/images/"):
     """
     Получает html код страницы.
     Через цикл сохраняет N изображений в папку с помощью функции store_image.
+
+    :param name: название поискового запроса
+    :param n: число изображений, которое нужно сохранить
+    :param path: путь к папке
+    :param URL: адрес страницы, с которой будут парситься изображения
     """
     index = 1
     page = 0
     html = requests.get(f"{URL}search?p={page}&text={name}&lr=51&rpt=image", headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"})
     soup = BS(html.text, 'lxml')
     all_images = soup.findAll("img")
+    os.mkdir(path)
     while (True):
         for image in all_images:
             if index > n:
@@ -52,12 +60,9 @@ def download_images(name, n, path):
         break
 
 if __name__ == "__main__":
-    URL = "https://yandex.ru/images/"
-    dataset_create("tiger", "leopard")
+    dataset_create()
     tiger_path = os.path.join('dataset', 'tiger')
-    os.mkdir(tiger_path)
     leopard_path = os.path.join('dataset', 'leopard')
-    os.mkdir(leopard_path)
     download_images("tiger", 999, tiger_path)
     download_images("leopard", 999, leopard_path)
     print("Изображения сохранены!")
